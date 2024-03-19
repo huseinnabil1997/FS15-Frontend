f9293f0a2c0f1ed4793423e0071a45fe2f7efaf9
huseinnabil <seinnabil@gmail.com>
2024-03-20 00:31:16 +0700

    Slicing register pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import Login from './pages/auth/Login'
+import Register from './pages/auth/Register'
+import ConfirmSucces from './pages/auth/ConfirmSuccess'
 import './App.css'
 
 function App() {
   return (
     <>
-      <Login />
+      <ConfirmSucces />
     </>
   )
 }
