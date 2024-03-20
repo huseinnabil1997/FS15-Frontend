c98f358e4dafc5a4ad2365516299ae60689745c0
huseinnabil <seinnabil@gmail.com>
2024-03-20 13:08:16 +0700

    Slicing Homepage

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import Login from './pages/auth/Login'
 import Register from './pages/auth/Register'
 import ConfirmSucces from './pages/auth/ConfirmSuccess'
+import Homepage from './pages/homepage'
 import './App.css'
+
 
 function App() {
   return (
     <>
-      <ConfirmSucces />
+      <Homepage />
     </>
   )
 }
---
+++ src/pages/homepage/index.jsx
@@ -0,0 +1,178 @@
+import React from 'react';
+import Header from '../../components/Header';
+import KijangInova from '../../assets/kijang_inova.png'
+import HondaBrio from '../../assets/honda_brio.png'
+import HyundaiPalisade from '../../assets/hyundai_palisade.png'
+import MitsubishiPajero from '../../assets/mitsubishi_pajero.png'
+import DumpTruck from '../../assets/dump_truck.png'
+import HondaCivic from '../../assets/honda_civic.png'
+import Benefit from '../../assets/benefit.png'
+import './homepage.css';
+
+import Electric from '../../assets/electric.png'
+import Hatchback from '../../assets/hatchback.png'
+import Lcgc from '../../assets/lcgc.png'
+import Mpv from '../../assets/mpv.png'
+import Offroad from '../../assets/offroad.png'
+import Sedan from '../../assets/sedan.png'
+import Suv from '../../assets/suv.png'
+import Truck from '../../assets/truck.png'
+
+const Homepage = () => {
+  const miniBoxes = [
+    {
+      id: 1,
+      name: 'Electric',
+      img: Electric,
+    },
+    {
+      id: 2,
+      name: 'Hatchback',
+      img: Hatchback,
+    },
+    {
+      id: 3,
+      name: 'LCGC',
+      img: Lcgc,
+    },
+    {
+      id: 4,
+      name: 'MPV',
+      img: Mpv,
+    },
+    {
+      id: 5,
+      name: 'Offroad',
+      img: Offroad,
+    },
+    {
+      id: 6,
+      name: 'Sedan',
+      img: Sedan,
+    },
+    {
+      id: 7,
+      name: 'SUV',
+      img: Suv,
+    },
+    {
+      id: 8,
+      name: 'Truck',
+      img: Truck,
+    },
+  ];
+  const boxes = [
+    {
+      id: 1,
+      category: 'SUV',
+      name: 'Course SUV Kijang Innova',
+      price: 'IDR 700.000',
+      img: KijangInova,
+    },
+    {
+      id: 2,
+      category: 'LCGC',
+      name: 'Course LCGC Honda Brio',
+      price: 'IDR 500.000',
+      img: HondaBrio,
+    },
+    {
+      id: 3,
+      category: 'SUV',
+      name: 'Hyundai Palisade 2021',
+      price: 'IDR 800.000',
+      img: HyundaiPalisade,
+    },
+    {
+      id: 4,
+      category: 'SUV',
+      name: 'Course Mitsubishi Pajero',
+      price: 'IDR 800.000',
+      img: MitsubishiPajero,
+    },
+    {
+      id: 5,
+      category: 'Truck',
+      name: 'Dump Truck for Mining Constructor',
+      price: 'IDR 1.200.000',
+      img: DumpTruck,
+    },
+    {
+      id: 6,
+      category: 'Sedan',
+      name: 'Sedan Honda Civic',
+      price: 'IDR 400.000',
+      img: HondaCivic,
+    },
+  ];
+  return (
+    <>
+      <Header homePage />
+      <div className="backgroundContainer">
+        <div className="textOverlay">
+          <div className='lessonTitle'>We provide driving lessons for various types of cars</div>
+          <div className='lessonSubtitle'>
+            Professional staff who are ready to help you to become a much-needed reliable driver
+          </div>
+          <div className='dataLessonContainer'>
+            <div className='boxLesson'>
+              <h1>50+</h1>
+              <p>A class ready to make you a reliable driver</p>
+            </div>
+            <div className='boxLesson'>
+              <h1>20+</h1>
+              <p>Professional workforce with great experience</p>
+            </div>
+            <div className='boxLesson'>
+              <h1>10+</h1>
+              <p>Cooperate with driver service partners</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="joinCourseBox">
+        <h1>Join us for the course</h1>
+        <div className='boxListContainer'>
+          {boxes.map(({ img, category, name, price }) => (
+            <div className="boxList" key={img}>
+              <img src={img} alt={name} />
+              <p className='carCategory'>{category}</p>
+              <h2>{name}</h2>
+              <p className='carPrice'>{price}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="benefitContainer">
+        <div className="benefitTextContainer">
+          <h1>Gets your best benefit</h1>
+          <p>
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam.
+          </p>
+          <p>
+            Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
+          </p>
+        </div>
+        <div className="benefitImageContainer">
+          <img style={{ zIndex: 2 }} src={Benefit} alt="Gets your best benefit" />
+          <div style={{ zIndex: -1 }} className='boxImageTop' />
+          <div style={{ zIndex: -1 }} className='boxImageBottom' />
+        </div>
+      </div>
+      <div className="moreCarType">
+        <h1>Join us for the course</h1>
+        <div className='boxListContainer'>
+          {miniBoxes.map(({ img, category, name, price }) => (
+            <div className="boxList" key={img}>
+              <img src={img} alt={name} />
+              <h2>{name}</h2>
+              <p className='carPrice'>{price}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Homepage;
