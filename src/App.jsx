867cf7e7eb8d4d205d62d67289fbbafd90a97053
huseinnabil <seinnabil@gmail.com>
2024-03-20 15:14:44 +0700

    Slicing Forgot Password

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import Register from './pages/auth/Register'
 import ConfirmSucces from './pages/auth/ConfirmSuccess'
 import Homepage from './pages/homepage'
 import Footer from './components/Footer'
+import ForgotPassword from './pages/auth/ForgotPassword'
 import './App.css'
 
 
 function App() {
   return (
     <>
-      <Homepage />
+      <ForgotPassword />
     </>
   )
 }
