59d38e643e2ea0e0d14e3fb89d2d3ff6dd27e391
huseinnabil <seinnabil@gmail.com>
2024-03-22 21:01:13 +0700

    Menyederhanakan code Route

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,52 @@ import NewPassword from './pages/auth/NewPassword';
 import MenuClass from './pages/MenuClass';
 import './App.css';
 
+const paths = [
+  {
+    pathName: '/',
+    compName: Homepage,
+  },
+  {
+    pathName: '/login',
+    compName: Login,
+  },
+  {
+    pathName: '/register',
+    compName: Register,
+  },
+  {
+    pathName: '/confirm-success',
+    compName: ConfirmSuccess,
+  },
+  {
+    pathName: '/forgot-password',
+    compName: ForgotPassword,
+  },
+  {
+    pathName: '/new-password',
+    compName: NewPassword,
+  },
+  {
+    pathName: '/menu-class',
+    compName: MenuClass,
+  },
+]
+
 function App() {
   return (
     <>
       <Router>
         <Switch>
-          <Route exact path="/login" component={Login} />
+          {paths?.map((data) => (
+            <Route exact path={data.pathName} component={data.compName} />
+          ))}
+          {/* <Route exact path="/login" component={Login} />
           <Route exact path="/register" component={Register} />
           <Route exact path="/confirm-success" component={ConfirmSuccess} />
           <Route exact path="/forgot-password" component={ForgotPassword} />
           <Route exact path="/new-password" component={NewPassword} />
           <Route exact path="/menu-class" component={MenuClass} />
-          <Route path="/" component={Homepage} />
+          <Route path="/" component={Homepage} /> */}
         </Switch>
       </Router>
     </>
