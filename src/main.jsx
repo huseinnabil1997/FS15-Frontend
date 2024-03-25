4afe67a41844c51025c69dbf2d28fe302b63372e
huseinnabil <seinnabil@gmail.com>
2024-03-25 20:44:02 +0700

    Responsive Footer

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+const link1 = document.createElement('link');
+link1.rel = 'stylesheet';
+link1.href = 'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&display=swap';
+document.head.appendChild(link1);
+
+const link2 = document.createElement('link');
+link2.rel = 'stylesheet';
+link2.href = 'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap';
+document.head.appendChild(link2);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
